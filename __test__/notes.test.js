c51babbf75b7d6c2e7d4c43921ce2467ebc9591f
Clayton Jones <claytonjonesLMP@gmail.com>
2020-03-17 14:14:21 -0700

    two tests for notes module prototypes pass

--- __test__/notes.test.js
+++ __test__/notes.test.js
@@ -1,12 +1,36 @@
 'use strict';
 
-function trueAsF() {
-  return true;
-}
+const Notes = require('../lib/notes.js');
 
-describe('Fake suite', () => {
-  it('TrueAsF', () => {
-    expect(trueAsF()).toBeTruthy();
-  });
+describe('Notes Module', () => {
+  it('Testing operation(): should call add() and output console log', () => {
+    let note = new Notes({action: 'add', payload: 'text'})
+    const spy = jest.spyOn(console, 'log');
+    const operation = jest.fn(note.operation());
+    operation();
 
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('Testing add(): should output console log', () => {
+    let note = new Notes({action: 'add', payload: 'text'})
+    const spy = jest.spyOn(console, 'log');
+    const add = jest.fn(note.add());
+    add();
+
+    expect(spy).toHaveBeenCalled();
+  })
 })
+
+
+
+// function trueAsF() {
+//   return true;
+// }
+
+// describe('Fake suite', () => {
+//   it('TrueAsF', () => {
+//     expect(trueAsF()).toBeTruthy();
+//   });
+
+// })
